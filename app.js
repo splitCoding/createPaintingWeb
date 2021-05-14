40df3f9c3ac76ee340f14849acc8462caf96f163
splitube <voicesplit@daum.net>
2021-05-14 16:05:22 +0900

    possible drawing on the canvas

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const canvas = document.querySelector('#jsCanvas');
 const ctx = canvas.getContext('2d');
+ctx.lineWidth = 2.5;
+ctx.strokeStyle = "black";
 
 
 let painting = false;
 
 function onMouseMove(e){
-  if(painting){
-    const x = e.offsetX;
-    const y = e.offsetY;
-    console.log(x,y);
+  const x = e.offsetX;
+  const y = e.offsetY;
+
+  if(!painting){
+    ctx.beginPath();
+    ctx.moveTo(x,y);
+  } else {
+    ctx.lineTo(x,y);
+    ctx.stroke();
   }
 }
 
