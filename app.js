89aab89450c275400b9da850fb3bcae1d472b930
splitube <voicesplit@daum.net>
2021-05-14 16:51:30 +0900

    possible to change colors of stroke

--- app.js
+++ app.js
@@ -1,23 +1,10 @@
 const canvas = document.querySelector('#jsCanvas');
 const ctx = canvas.getContext('2d');
+const colors = document.querySelectorAll(".jsColors");
 ctx.lineWidth = 2.5;
 ctx.strokeStyle = "black";
 
-
 let painting = false;
-
-function onMouseMove(e){
-  const x = e.offsetX;
-  const y = e.offsetY;
-
-  if(!painting){
-    ctx.beginPath();
-    ctx.moveTo(x,y);
-  } else {
-    ctx.lineTo(x,y);
-    ctx.stroke();
-  }
-}
 
 function startPainting(e){
   painting = true;
@@ -27,6 +14,18 @@ function stopPainting(){
   painting = false;
 }
 
+function onMouseMove(e){
+  const x = e.offsetX;
+  const y = e.offsetY;
+  
+  if(!painting){
+    ctx.beginPath();
+    ctx.moveTo(x,y);
+  } else {
+    ctx.lineTo(x,y);
+    ctx.stroke();
+  }
+}
 
 if(canvas){
   canvas.addEventListener('mousemove', onMouseMove);
@@ -34,3 +33,9 @@ if(canvas){
   canvas.addEventListener('mouseup', stopPainting);
   canvas.addEventListener('mouseleave', stopPainting);
 }
+
+function changeColor(e){
+  ctx.strokeStyle = e.target.style.backgroundColor;
+}
+
+Array.from(colors).forEach(function(){addEventListener('click',changeColor)});
